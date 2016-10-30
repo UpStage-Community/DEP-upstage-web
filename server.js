2a29b51e507f1e6deb08ff3680a9abd5c5632671
Majd Murad <murad.majd@gmail.com>
2016-10-29 19:16:31 -0700

    Added webpack dev middleware to run from express server and allow reloading.

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
+// The following was influenced by https://github.com/christianalfoni/webpack-express-boilerplate
+
 const express = require('express')
 const path = require('path')
-const app = express()
-const port = process.env.PORT || 8080
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const config = require('./webpack.config.js');
 
-app.use(express.static(__dirname + '/app'));
+const debug = process.env.NODE_ENV !== 'production';
+const port = debug ? 8080 : process.env.PORT;
+const app = express();
+
+if (debug) {
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'app',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+
+  app.use(middleware);
+
+} else {
+  app.use(express.static(__dirname + '/app'));
+
+}
 
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'app', 'index.html'))
+    response.sendFile(path.resolve(__dirname, 'app', 'index.html'))
 })
 
-app.listen(port);
-
-console.log(`Listening at http://localhost:${port}`)
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info(`==> 🌎  Listening at http://localhost:${port}`);
+});
